3bc0221852c6a474e2d984d4041c0e740a790a54
Mohana <mpkulkarni117@gmail.com>
2023-07-22 15:05:41 +0530

    Round Button Added to card

--- components/meal.php
+++ components/meal.php
@@ -16,6 +16,7 @@
     if (count($all_cards) != 0) {
       foreach ($all_cards as &$card) {
         if ($card['id'] == $newId) {
+          
           array_push($card['tags'], $row['name']);
           $card_present = true;
           break;
@@ -36,6 +37,8 @@
       array_push($all_cards, $card_data);
     }
   }
+  
+  //print_r($all_cards);
 
   foreach ($all_cards as $row) {
     $title = $row['title'];
@@ -49,9 +52,9 @@
     ?>
 
     <div class="meal">
-    <img src="img/meals/<?php echo $img_name; ?>.jpg" class="meal-img" alt="<?php echo $title; ?>" />
-    <div class="meal-content">
-      <div class="meal-tags">
+      <img src="img/meals/<?php echo $img_name; ?>.jpg" class="meal-img" alt="<?php echo $title; ?>" />
+      <div class="meal-content">
+        <div class="meal-tags">
         <?php 
           foreach ($tags as $tag) {
             ?>
@@ -60,29 +63,30 @@
           }
         ?>
         </div>
-      <div>
           
-      </div>
-      <p class="meal-title"><?php echo $title; ?></p>
-      <ul class="meal-attributes">
-        <li class="meal-attribute">
-          <ion-icon class="meal-icon" name="flame-outline"></ion-icon><span> <strong><?php echo $calories." "; ?></strong>Calories</span>
-        </li>
-        <li class="meal-attribute">
-          <ion-icon class="meal-icon" name="restaurant-outline"></ion-icon><span> Nutriscore &reg;
-            <strong><?php echo $nutriscore; ?></strong></span>
-        </li>
-        <li class="meal-attribute">
-          <ion-icon class="meal-icon" name="star-outline"></ion-icon><span> <strong><?php echo $ratings; ?></strong> rating (<?php echo $votes; ?>)</span>
-        </li>
-      </ul>
-      <div class="card-price-div">
-        <strong>
-          <span class="card-price" >&#8377;<?php echo $price ?></span>
-        </strong>
+        <div>
+          <p class="meal-title"><?php echo $title; ?></p>
+          <ul class="meal-attributes">
+            <li class="meal-attribute">
+              <ion-icon class="meal-icon" name="flame-outline"></ion-icon><span> <strong><?php echo $calories." "; ?></strong>Calories</span>
+            </li>
+            <li class="meal-attribute">
+              <ion-icon class="meal-icon" name="restaurant-outline"></ion-icon><span> Nutriscore &reg;
+                <strong><?php echo $nutriscore; ?></strong></span>
+            </li>
+            <li class="meal-attribute">
+              <ion-icon class="meal-icon" name="star-outline"></ion-icon><span> <strong><?php echo $ratings; ?></strong> rating (<?php echo $votes; ?>)</span>
+            </li>
+          </ul>
+          <div class="card-grid card-price-div">
+            <strong>
+              <span class="card-price" >&#8377;<?php echo $price ?></span>
+            </strong>
+            <button class="btn-round">+</button>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
     <?php
   }
 ?>
