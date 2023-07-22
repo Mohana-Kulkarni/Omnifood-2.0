d46eea218d6dd6af3663ad187b9e9c270878855b
Jay Shitre <jayshitre@gmail.com>
2023-07-22 23:22:39 +0530

    added cart page

---
+++ cart.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="description"
+    content="Omnifood is an AI-powered food subscription that will make you eat healthy again, 365  days per year. It's tailored to  your personal tastes and nutritional needs."
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="icon" href="img/favicon.png" />
+  <link rel="apple-touch-icon" href="img/favicon-apple-touch.png" />
+  <link rel="manifest" href="manifest.webmanifest" />
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="css/general.css" />
+
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="css/queries.css" />
+  <title>Omnifood &mdash; Cart</title>
+</head>
+
+<body>
+  <header class="header">
+    <a href="#">
+      <img class="logo" alt="Omnifood logo" src="img/omnifood-logo.png" />
+    </a>
+    <nav class="main-nav">
+      <ul class="main-nav-list">
+        <li>
+          <a class="main-nav-link" href="index.html"> How it works</a>
+        </li>
+        <li>
+          <a class="main-nav-link" href="meals.php"> Meals</a>
+        </li>
+        <li>
+          <a class="main-nav-link" href="#testimonials"> Testimonials </a>
+        </li>
+        <li>
+          <a class="main-nav-link" href="#pricing"> Pricing</a>
+        </li>
+        <li>
+          <a class="main-nav-link nav-cta" href="#cta"> Try for free</a>
+        </li>
+      </ul>
+    </nav>
+    <button class="btn-mobile-nav">
+      <ion-icon class="icon-mobile-nav" name="menu-outline"></ion-icon>
+      <ion-icon class="icon-mobile-nav" name="close-outline"></ion-icon>
+    </button>
+  </header>
+
+  <section class="section-meals" id="meals">
+    <div class="container center-text">
+      <span class="subheading"> Cart</span>
+      <h2 class="heading-secondary">
+        Your Shopping List
+      </h2>
+    </div>
+
+    <div class="container cart-grid grid--2-cols margin-bottom-md">
+      <?php include "components/cart.php"; ?>
+
+    </div>
+  </section>
+
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+  <script defer src="https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+    "></script>
+
+</body>
+
+</html>
---
+++ components/cart.php
@@ -0,0 +1,67 @@
+<?php 
+  include "db.php";
+  include "models/meal_model.php";
+  include "controllers/meal_cntr.php";
+  
+  $all_cards = meal_data($con);
+
+  foreach ($all_cards as $row) {
+    $title = $row['title'];
+    $calories = $row['calories'];
+    $nutriscore = $row['nutriscore'];
+    $ratings = $row['ratings'];
+    $votes = $row['votes'];
+    $img_name = $row['image_name'];
+    $price = $row['price'];
+    $tags = $row['tags'];
+    ?>
+
+    <div class="cart-meal">
+      <div class="cart-img-div">
+        <img src="img/meals/<?php echo $img_name; ?>.jpg" class="cart-meal-img" alt="<?php echo $title; ?>" />
+      </div>
+      <div class="cart-content">
+        
+        <p class="cart-meal-title"><?php echo $title; ?></p>
+        <div class="cart-meal-tags">
+        <?php 
+          foreach ($tags as $tag) {
+            ?>
+            <span class="tag tag--<?php echo strtolower($tag) ?>"><?php echo $tag ?></span>
+            <?php
+          }
+        ?>
+        </div>
+        <div>
+          <ul class="cart-meal-attributes">
+            <div class="meal-sub-attributes">
+                <li class="cart-meal-attribute">
+                  <ion-icon class="meal-icon" name="flame-outline"></ion-icon><span> <strong><?php echo $calories." "; ?></strong>
+                </li>
+                <li class="cart-meal-attribute">
+                  <ion-icon class="meal-icon" name="restaurant-outline"></ion-icon><span>
+                    <strong><?php echo $nutriscore; ?></strong></span>
+                </li>
+                <li class="cart-meal-attribute">
+                  <ion-icon class="meal-icon" name="star-outline"></ion-icon><span> <strong><?php echo $ratings; ?></strong> (<?php echo $votes; ?>)</span>
+                </li>
+            </div>
+          </ul>
+          <div class="card-grid cart-card-price-div">
+            <strong>
+              <span class="card-price" >&#8377;<?php echo $price ?></span>
+            </strong>
+            <div class="cart-btn-div">
+                <button class="cart-btn-round-left"><img class="subtract-icon" src="img/icons/subtract.svg" alt="subtract-icon" /></button>
+                <li class="cart-btn-value">2</li>
+                <button class="cart-btn-round-right"><img class="add-icon" src="img/icons/add.svg" alt="add-icon" /></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <?php
+  }
+?>
+
+  
