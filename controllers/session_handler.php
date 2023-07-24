73d43ce45594a96125053b9348a50935dc68bec1
Mohana <mpkulkarni117@gmail.com>
2023-07-24 22:51:56 +0530

    Implemented cart using AJAX

--- controllers/session_handler.php
+++ controllers/session_handler.php
@@ -4,11 +4,16 @@
   $id = $_GET['id'];
   if(!in_array($id, $_SESSION['cart'])) {
     $_SESSION['cart'][$id] =  $id;
-    echo "SUCCESS";  
+    $my_obj = new stdClass();
+    $my_obj->result = "SUCCESS"; 
+    echo json_encode($my_obj);
   } else {
+    $my_obj = new stdClass();
+    $my_obj->result = "FAILURE"; 
+    echo json_encode($my_obj);
 
     unset($_SESSION['cart'][$id]);
-
+    
     //array_pop($_SESSION['cart'], $id);
   }
   
--- js/cart_script.js
+++ js/cart_script.js
@@ -1,16 +1,23 @@
 
 function add_to_cart(id) {
   
-  const btn_cart = document.getElementById(id).classList;
-  btn_cart.add("hide-btn");
+  const btn_cart = document.getElementById(id);
   
   var xhr = new XMLHttpRequest();
+  var res = "";
   xhr.open("GET", "./controllers/session_handler.php?id="+id, true); 
   xhr.onreadystatechange = function() {
     if(xhr.readyState == 4 && xhr.status == 200) {
-      console.log(this.responseText);
+      res = this.responseText;
+      res = JSON.parse(res);  
+      console.log(res.result);  
+      if(res.result === "SUCCESS") {
+        btn_cart.innerHTML = "<img class='add-icon' src='img/icons/subtract.svg' alt='add-icon' />";
+      } else {
+        btn_cart.innerHTML = "<img class='add-icon' src='img/icons/add.svg' alt='add-icon' />";
+      }
+      
     }
   }
   xhr.send();
-  
 }
